485148da5c04be0bd93dbc5dbdca457ff7db6d19
vedro-compota <ru-ibn-hottab@yandex.ru>
2021-08-11 13:39:24 +0300

    Get params validation example

--- src/controllers/test/test.controller.ts
+++ src/controllers/test/test.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import 'joi-extract-type';
+import * as joi from 'joi';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { JoiValidationPipe } from '../../pipes/validation.pipe';
+
+enum MyType {
+  FIRST = 'first',
+  SECOND = 'second',
+}
+
+export const testGetParamsSchema = joi.object({
+  type: joi.string().valid(MyType.FIRST, MyType.SECOND).required(),
+});
+
+export type TestGetParamsSchema = joi.extractType<typeof testGetParamsSchema>;
 
 @Controller('test')
 export class TestController {
   @Get()
   getHello(): string {
     return 'Test';
+  }
+
+  @Get('getid/:id')
+  getUrlParam(@Param('id') id): string {
+    return 'Test param:' + id;
+  }
+
+  @Get('queryparams/:id')
+  getTestGetParams(
+    @Param('id') id,
+    @Query('type') type,
+    @Query(new JoiValidationPipe(testGetParamsSchema)) //new JoiValidationPipe(testGetParamsSchema)
+    query: TestGetParamsSchema,
+  ): string {
+    testGetParamsSchema.validate(query);
+    console.log('-------------query', query);
+    console.log('-------------valid', testGetParamsSchema.validate(query));
+    return `Url param: ${id}, Get param type = ${type}`;
   }
 }
